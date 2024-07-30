5f4f0b96620811b98bdbc90819fb7705b0bc73ba
TheUltimateNuke <nuke.public@gmail.com>
2024-07-30 15:55:46 -0400

    script fixes

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir ../build/
-mkdir ../build/docs/
-mkdir ../build/api/
-cp -r ./pages/landing ../build/
-cp -r ./pages/docs/ ../build/docs/
-cp -r ./pages/api/_site/ ../build/api/
+mkdir ./build/
+cp -rf ./pages/landing/ ./build
+cp -rf ./pages/docs ./build/docs
+cp -rf ./pages/api/_site ./build/api
 # tar -czvf build.tar.gz ../build/ || exit 1
