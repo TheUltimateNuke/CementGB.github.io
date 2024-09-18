674bb7a90ce0d4784ff0072092c681a2f8885779
TheUltimateNuke <nuke.public@gmail.com>
2024-09-18 15:33:54 +0000

    made paths relative to script instead of cd

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" || exit
+
+dotnet tool update -g docfx
+docfx "$SCRIPTPATH/../pages/api/docfx.json"
+bash "$SCRIPTPATH/pack.sh"
--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-cp -rf ./pages/landing/ ./build
-cp -rf ./pages/docs ./build/docs
-cp -rf ./pages/api/_site ./build/api
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" || exit
+
+cp -rf "$SCRIPTPATH/../pages/landing/" "$SCRIPTPATH/../build"
+cp -rf "$SCRIPTPATH/../pages/docs" "$SCRIPTPATH/../build/docs"
+cp -rf "$SCRIPTPATH/../pages/api/_site" "$SCRIPTPATH/../build/api"
 # tar -czvf build.tar.gz ../build/ || exit 1
