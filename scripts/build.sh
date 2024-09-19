03aa2e5f631c9d668e6975e41c52053426ce7bab
TheUltimateNuke <nuke.public@gmail.com>
2024-09-19 15:03:38 +0000

    clarified build process with comments

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
+# Saves the script's current path
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" || exit
 
+# Install and fix PATH for docfx
 dotnet tool update -g docfx
+export PATH="$HOME/.dotnet/tools:$PATH"
+
+# Build docs
 docfx "$SCRIPTPATH/../pages/docs/docfx.json"
+
+# Pack everything together nicely for deployment
 bash "$SCRIPTPATH/pack.sh"
--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# Saves the script's current path
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" || exit
 
+# Remove previous build
 rm -rf "$SCRIPTPATH/../build/"
+
+# Pack pages into build
 cp -rf "$SCRIPTPATH/../pages/landing/" "$SCRIPTPATH/../build"
 cp -rf "$SCRIPTPATH/../pages/docs/_site" "$SCRIPTPATH/../build/docs"
+
+# Compress build to tar.gz (unused)
 # tar -czvf build.tar.gz ../build/ || exit 1
